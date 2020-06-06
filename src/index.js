82dcc90b46e1fb7e40daa091ee2bd0a42b564efe
Justin <JEStaubach@users.noreply.github.com>
2020-06-06 09:00:43 -0400

    remove engine param

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const { typeDefs } = require("./schema");
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  engine: { apiKey: process.env.ENGINE_API_KEY },
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
