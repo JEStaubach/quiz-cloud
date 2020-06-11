e76b1291fb2e060faef05126ad897d12fad6d270
Justin <JEStaubach@users.noreply.github.com>
2020-06-11 01:00:46 -0400

    ...

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction || { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = { pool };
